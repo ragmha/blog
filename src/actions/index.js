12b51de14db20bd72739ee41fa82046e17164980
ragmha <Abul.MidhatHasanRaghib@metropolia.fi>
2017-07-03 13:46:28 +0300

    add render posts

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,6 +4,7 @@ import { FETCH_POSTS, ROOT_URL, API_KEY } from "../constants";
 
 export function fetchPosts() {
   const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
+  console.log("Request: ", request);
   return {
     type: FETCH_POSTS,
     payload: request
