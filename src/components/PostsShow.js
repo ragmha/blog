d0852964188c0891a3128149d2e641bd01581e26
ragmha <Abul.MidhatHasanRaghib@metropolia.fi>
2017-07-04 12:21:08 +0300

    selecting post with OwnProps

--- src/components/PostsShow.js
+++ src/components/PostsShow.js
@@ -1,8 +1,21 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetchPost } from "../actions";
 
 class PostsShow extends Component {
+  componentDidMount() {
+    const { id } = this.props.match.params; // provided by react-router :D
+    this.props.fetchPost(id);
+  }
   render() {
     return <div>Post Show!</div>;
   }
 }
-export default PostsShow;
+
+function mapStateToProps({ posts }, ownProps) {
+  return {
+    post: posts[ownProps.match.params.id]
+  };
+}
+
+export default connect(null, { fetchPost })(PostsShow);
